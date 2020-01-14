56d4c53b26d2d99484a3dc44e22603f9219bed9a
Diego F <diego.flores@n12.mx>
2020-01-14 12:49:28 -0600

    animation refresh

--- templatedos/src/sections/sect3.js
+++ templatedos/src/sections/sect3.js
@@ -1,30 +1,27 @@
 import React, {Component} from 'react';
-import {Icon} from 'antd';
+import {Container, Col,Row} from 'reactstrap';
+import imga1 from "../images/img/imgnref.jpg";
 import '../style/sect3.css';
 
 export default class Sect3 extends Component{
-  constructor(props){
-    super(props);
-    this.wrapperRef = React.createRef();
-  }
-  handleClick(){
-    const wrapper =this.wrapperRef.current;
-    wrapper.classList.toggle('is-nave-open')
-  }
   render(){
     return(
-      <div ref={this.wrapperRef} className="wrapper">
-        <div className="nave">
-          <Icon
-            className="nave-icon"
-            type="menu-fold"
-            onClick={()=> this.handleClick()}/>
-
-            <div className = "nave-body">
-            ASDFGÑLKJHG
+      <div className="bodie">
+<Container>
+  <Col>
+    <Row>
+          <div className="textform">
+            Cat ipsum dolor sit amet, purrr purr littel cat, little cat purr purr give me attention or face the wrath of my claws, refuse to leave cardboard box. Cat is love, cat is life meow for food, then when human fills food dish, take a few bites of food and continue meowing for licks your face. Cereal boxes make for five star accommodation lick butt, so meow loudly just to annoy owners good now the other hand, too for meow meowing chowing and wowing sit by the fire. Sit in box jump off balcony, onto stranger's head so throw down all the stuff in the kitchen poop in a handbag look delicious and drink the soapy mopping up water then puke giant foamy fur-balls cough hairball on conveniently placed pants climb a tree
             </div>
-        </div>
-      </div>
+
+            <img src={imga1}
+              className="nave-img"
+              type="menu-fold"
+              alt="" />
+    </Row>
+  </Col>
+</Container>
+</div>
     );
   }
 }
